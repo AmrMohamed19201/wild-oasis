9e8ba5c3228becdb330a86da0d3f5c6198f670d5
amr <amr.mohamed19201@gmail.com>
2026-02-01 13:45:17 +0200

    Building the Sidebar and Main Navigation

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,12 +1,20 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import {
+  HiOutlineCalendarDays,
+  HiOutlineCog6Tooth,
+  HiOutlineHome,
+  HiOutlineHomeModern,
+  HiOutlineUsers,
+} from "react-icons/hi2";
 
 const NavList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
 `;
-
-const Link = styled.a`
+//THIS WAY TO USE STYLED COMPONENT WITH REACT ROUTER ELEMENTS (NOT HTML ELEMENT)
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +52,33 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <StyledNavLink to="/dashboard">
+          <HiOutlineHome />
+          <span>Home</span>
+        </StyledNavLink>
+        <StyledNavLink to="/bookings">
+          <HiOutlineCalendarDays />
+          <span>Bookings</span>
+        </StyledNavLink>
+        <StyledNavLink to="/cabins">
+          <HiOutlineHomeModern />
+          <span>Cabins</span>
+        </StyledNavLink>
+        <StyledNavLink to="/users">
+          <HiOutlineUsers />
+          <span>Users</span>
+        </StyledNavLink>
+        <StyledNavLink to="/settings">
+          <HiOutlineCog6Tooth />
+          <span>Settings</span>
+        </StyledNavLink>
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
