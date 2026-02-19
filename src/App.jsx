e98756c38f4eadda751c68852e6c6eb1abedfa7f
amr <amr.mohamed19201@gmail.com>
2026-02-20 00:24:57 +0200

    deleting a cabin

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
